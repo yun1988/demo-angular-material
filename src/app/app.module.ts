472f13ab412c82a1212ea257976278c51547e30e
“yun1988” <small2883@hotmail.com>
2018-03-02 18:09:18 +0800

    AppModule中的imports裡面加入BrowserAnimationsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
